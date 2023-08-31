7f8d47d905848fc697abe2876df8798e086542a3
RafaelNiccheri <rafaelpn1@al.insper.edu.br>
2023-08-31 11:41:22 -0300

    commilis

--- PIO-IRQ/src/main.c
+++ PIO-IRQ/src/main.c
@@ -45,7 +45,8 @@
 /************************************************************************/
 /* variaveis globais                                                    */
 /************************************************************************/
-
+/* flag */
+volatile char but_flag; // (1)
 /************************************************************************/
 /* prototype                                                            */
 /************************************************************************/
@@ -63,9 +64,8 @@ void pisca_led(int n, int t);
  * !! Isso é um exemplo ruim, nao deve ser feito na pratica, !!
  * !! pois nao se deve usar delays dentro de interrupcoes    !!
  */
-void but_callback(void)
-{
-  pisca_led(5, 200);
+void but_callBack(void){
+	but_flag = 1;
 }
 
 /************************************************************************/
@@ -95,7 +95,8 @@ void io_init(void)
 
   // Configura PIO para lidar com o pino do botão como entrada
   // com pull-up
-	pio_configure(BUT_PIO, PIO_INPUT, BUT_IDX_MASK, PIO_PULLUP);
+	pio_configure(BUT_PIO, PIO_INPUT, BUT_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	pio_set_debounce_filter(BUT_PIO, BUT_IDX_MASK, 60);
 
   // Configura interrupção no pino referente ao botao e associa
   // função de callback caso uma interrupção for gerada
@@ -103,8 +104,8 @@ void io_init(void)
   pio_handler_set(BUT_PIO,
                   BUT_PIO_ID,
                   BUT_IDX_MASK,
-                  PIO_IT_FALL_EDGE,
-                  but_callback);
+                  PIO_IT_RISE_EDGE,
+                  but_callBack);
 
   // Ativa interrupção e limpa primeira IRQ gerada na ativacao
   pio_enable_interrupt(BUT_PIO, BUT_IDX_MASK);
@@ -134,7 +135,13 @@ void main(void)
 
 	// super loop
 	// aplicacoes embarcadas no devem sair do while(1).
-	while(1)
-  {
+	while(1){
+		if (but_flag) {  // (2)
+			pisca_led(5, 200);
+			but_flag = 0; // (3)
+		}
+		// Entra em sleep mode
+		pmc_sleep(SAM_PM_SMODE_SLEEP_WFI); // (1)
+
 	}
 }
